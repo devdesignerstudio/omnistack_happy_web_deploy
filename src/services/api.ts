e144086c5dd269d2752c1c55c4769a01bd2c8e13
Celio Maymone <devdesignerstudio@protonmail.com>
2020-10-26 17:56:00 -0300

    mudando baseURL

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://happy-omnistack-deploy.herokuapp.com',
+  baseURL: 'https://happybrazil.netlify.app',
 });
 
 export default api;
